602e911ca1cda3ed3e89f07b248b228e1b6d1e2a
dvd <david@chartbeat.com>
2014-10-24 10:23:36 -0400

    testing camera

---
+++ camera-test.py
@@ -0,0 +1,6 @@
+import cv
+capture = cv.CaptureFromCAM(-1)
+cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_WIDTH, 960)
+cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_HEIGHT, 720)
+im = cv.QueryFrame(capture)
+cv.SaveImage("/home/pi/camera-test.jpg", im)
